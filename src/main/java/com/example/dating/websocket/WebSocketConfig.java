34feff6ab6186bc1bb41eecd9b7d295c99a45f2b
qlsod <qlsod9570@naver.com>
2024-03-20 15:08:13 +0900

    연결확인용

--- src/main/java/com/example/dating/websocket/WebSocketConfig.java
+++ src/main/java/com/example/dating/websocket/WebSocketConfig.java
@@ -17,6 +17,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
         // "/ws/chat" - WebSocket 연결이 설정될 엔드포인트. 이 경로로 요청하면 웹소켓 핸드쉐이킹
         // setAllowedOrigins("*") - 모든 CORS 요청 혀용
         // 정리하자면 클라이언트가 "/ws/chat" 경로로 요청하면 webSocketHandler 에서 제공하는 웹소켓 기능을 사용할 수 있다.
+
         registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
 
     }

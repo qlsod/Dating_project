439920063e9926903f934a8d9ac57098d553a4dc
qlsod <qlsod9570@naver.com>
2024-04-18 18:21:57 +0900

    따로 저장된 프로필 이미지들 get 메소드 수정

--- src/main/java/com/example/dating/service/MemberService.java
+++ src/main/java/com/example/dating/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.dating.service;
 import com.example.dating.domain.Block;
 import com.example.dating.domain.HumanMember;
 import com.example.dating.domain.Member;
+import com.example.dating.domain.ProfileImage;
 import com.example.dating.dto.block.BlockListDto;
 import com.example.dating.dto.email.EmailDto;
 import com.example.dating.dto.member.*;
@@ -10,9 +11,11 @@ import com.example.dating.mbti.Mbti;
 import com.example.dating.repository.BlockRepository;
 import com.example.dating.repository.HumanMemberRepository;
 import com.example.dating.repository.MemberRepository;
+import com.example.dating.repository.ProfileImagesRepository;
 import com.example.dating.security.jwt.TokenInfo;
 import com.example.dating.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -21,12 +24,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -34,6 +39,7 @@ public class MemberService {
     private final TokenProvider tokenProvider;
     private final BlockRepository blockRepository;
     private final HumanMemberRepository humanMemberRepository;
+    private final ProfileImagesRepository profileImagesRepository;
 
     @Transactional
     public Long join(MemberJoinDto memberJoinDto) throws RuntimeException {
@@ -85,18 +91,45 @@ public class MemberService {
      * 입력한 회원 정보 저장
      */
     @Transactional
-    public void save(Long memberId, MemberInfoDto memberInfoDto) {
-        Optional<Member> memberOptional = memberRepository.findById(memberId);
+    public void save(String email, MemberInfoDto memberInfoDto) {
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
 
-       memberOptional.ifPresentOrElse(member -> member.mapDtoToEntity(memberInfoDto),
+        memberOptional.ifPresentOrElse(member -> member.mapDtoToEntity(memberInfoDto),
                () -> { throw new RuntimeException("회원이 존재하지 않습니다."); });
     }
+
+
+    /**
+     * 입력한 프로필 사진들 저장
+     */
+    @Transactional
+    public void saveProfileImages(String email, MemberInfoDto memberInfoDto) {
+
+        // 이메일로 회원 조회
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> { throw new RuntimeException("회원이 존재하지 않습니다."); });
+
+        // 이미지 URL 리스트 가져오기
+        List<String> imageUrls = memberInfoDto.getImages();
+
+        // 이미지 URL들을 ProfileImage 엔티티로 생성하여 저장
+        List<ProfileImage> profileImages = imageUrls.stream()
+                .map(imageUrl -> new ProfileImage(member, imageUrl))
+                .collect(Collectors.toList());
+        log.info("3");
+
+        profileImagesRepository.saveAll(profileImages);
+    }
+
+
 
     /**
      * 20명의 랜덤 이성 회원을 추천
      */
     public List<MemberCardDto> getRandomMemberList(String email) {
         Member findMember = memberRepository.findByEmail(email).get();
+
+        log.info(String.valueOf(findMember));
 
         PageRequest pageRequest = PageRequest.of(0, 20);
         return memberRepository.findRandomMember(findMember.getId(), findMember.getGender(), pageRequest);
@@ -127,8 +160,18 @@ public class MemberService {
     public MemberInfoDto getMemberProfile(String email) {
         Member findMember = memberRepository.findByEmail(email).get();
 
+        List<ProfileImage> profileImages = profileImagesRepository.findAllByMemberId(findMember.getId());
+
+        // 이미지 URL 리스트를 생성하여 DTO에 추가
+        List<String> imageUrls = new ArrayList<>();
+        for (ProfileImage profileImage : profileImages) {
+            imageUrls.add(profileImage.getImage());
+        }
+
         MemberInfoDto memberInfoDto = new MemberInfoDto();
         memberInfoDto.mapEntityToDto(findMember);
+        memberInfoDto.setImages(imageUrls);
+
         return memberInfoDto;
     }
 

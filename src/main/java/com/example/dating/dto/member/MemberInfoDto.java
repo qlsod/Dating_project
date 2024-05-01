cb61f01035593bb0e2bc1d5fcb777e5c6df5d0ce
qlsod <qlsod9570@naver.com>
2024-05-01 15:54:29 +0900

    Entity name 필드의 이름 변경 name -> nickName, comment -> description, residence -> address 및 gender 필드 추가

--- src/main/java/com/example/dating/dto/member/MemberInfoDto.java
+++ src/main/java/com/example/dating/dto/member/MemberInfoDto.java
@@ -1,11 +1,14 @@
 package com.example.dating.dto.member;
 
 import com.example.dating.domain.Member;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -13,50 +16,59 @@ import java.util.List;
 public class MemberInfoDto {
 
     @NotEmpty(message = "이름을 입력해주세요")
-    private String name;
+    private String nickName;
 
-    @NotEmpty(message = "한줄 소개를 입력해주세요")
-    private String comment;
+//    @NotEmpty(message = "한줄 소개를 입력해주세요")
+    private String description;
+
+    @NotNull(message = "생일을 입력해주세요")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDay;
 
     @NotEmpty(message = "사는 곳을 입력해주세요")
-    private String residence;
+    private String address;
 
-    @NotNull(message = "나이를 입력해주세요")
+    @NotEmpty(message = "성별을 입력해주세요")
+    private String gender;
+
+//    @NotNull(message = "나이를 입력해주세요")
     private int age;
 
-    @NotNull(message = "키를 입력해주세요")
+//    @NotNull(message = "키를 입력해주세요")
     private int height;
 
-    @NotEmpty(message = "사진을 입력해주세요")
+//    @NotEmpty(message = "사진을 입력해주세요")
     private List<String> images;
 
-    @NotEmpty(message = "대표 이미지를 입력해주세요")
+//    @NotEmpty(message = "대표 이미지를 입력해주세요")
     private String image;
 
-    @NotEmpty(message = "인적사항을 입력해주세요")
+//    @NotEmpty(message = "인적사항을 입력해주세요")
     private String personalInfo;
 
-    @NotEmpty(message = "mbti를 입력해주세요")
-    private String mbti;
+//    @NotEmpty(message = "mbti를 입력해주세요")
+//    private String mbti;
 
-    @NotEmpty(message = "성격을 입력해주세요")
+//    @NotEmpty(message = "성격을 입력해주세요")
     private String personality;
 
-    @NotEmpty(message = "관심사를 입력해주세요")
+//    @NotEmpty(message = "관심사를 입력해주세요")
     private String interest;
 
-    @NotEmpty(message = "이상형을 입력해주세요")
+//    @NotEmpty(message = "이상형을 입력해주세요")
     private String likePersonality;
 
     public void mapEntityToDto(Member member) {
-        this.name = member.getName();
-        this.comment = member.getComment();
-        this.residence = member.getResidence();
+        this.nickName = member.getNickName();
+        this.description = member.getDescription();
+        this.birthDay = member.getBirthDay();
+        this.address = member.getAddress();
         this.age = member.getAge();
         this.height = member.getHeight();
         this.image = member.getImage();
         this.personalInfo = member.getPersonalInfo();
-        this.mbti = member.getMbti();
+        this.gender = member.getGender();
+//        this.mbti = member.getMbti();
         this.personality = member.getPersonality();
         this.interest = member.getInterest();
         this.likePersonality = member.getLikePersonality();

5cf6ba7a89fb341bf7a1ce3bcc41faa9e301cbbe
qlsod <qlsod9570@naver.com>
2024-05-01 15:53:52 +0900

    Entity name 필드의 이름 변경 name -> nickName, comment -> description, residence -> address

--- src/main/java/com/example/dating/domain/Member.java
+++ src/main/java/com/example/dating/domain/Member.java
@@ -1,9 +1,12 @@
 package com.example.dating.domain;
 
 import com.example.dating.dto.member.MemberInfoDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -15,30 +18,35 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "MEMBER_ID")
     private Long id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDay;
     private String email;
     private String password;
-    private String name;
-    private String comment;
+    private String nickName;
+    private String description;
     private String gender;
-    private String residence;
+    private String address;
     private Integer age;
     private Integer height;
     private String image;
     private String personalInfo;
-    private String mbti;
+//    private String mbti;
     private String personality;
     private String interest;
     private String likePersonality;
 
     public void mapDtoToEntity(MemberInfoDto memberInfoDto) {
-        this.name = memberInfoDto.getName();
-        this.comment = memberInfoDto.getComment();
-        this.residence = memberInfoDto.getResidence();
+        this.nickName = memberInfoDto.getNickName();
+        this.description = memberInfoDto.getDescription();
+        this.address = memberInfoDto.getAddress();
         this.age = memberInfoDto.getAge();
         this.height = memberInfoDto.getHeight();
         this.image = memberInfoDto.getImage();
+        this.birthDay = memberInfoDto.getBirthDay();
         this.personalInfo = memberInfoDto.getPersonalInfo();
-        this.mbti = memberInfoDto.getMbti();
+//        this.mbti = memberInfoDto.getMbti();
+        this.gender = memberInfoDto.getGender();
         this.personality = memberInfoDto.getPersonality();
         this.interest = memberInfoDto.getInterest();
         this.likePersonality = memberInfoDto.getLikePersonality();

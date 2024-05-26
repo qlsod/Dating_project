06bb35cd7bad4bb570e40433d88310b9af09880b
qlsod <rlatpals100@naver.com>
2024-05-26 19:01:11 +0900

    탐색 api 생성에 따른 security permitAll 추가

--- src/main/java/com/example/dating/security/config/SecurityConfig.java
+++ src/main/java/com/example/dating/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.dating.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -37,6 +38,7 @@ public class SecurityConfig {
                                 .antMatchers("/member/mail/confirm").permitAll()
                                 .antMatchers("/refresh").permitAll()
                                 .antMatchers("/ws/**").permitAll()
+                                .antMatchers(HttpMethod.GET, "/search/**").permitAll() // GET 요청 허용
                                 .antMatchers("/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
                                         "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
                                         "/webjars/**", "/swagger-ui.html").permitAll()

9a575ff1cfff5bf7d0ddffb65cf750de64c17de2
qlsod <rlatpals100@naver.com>
2024-05-26 18:59:47 +0900

    탐색 api Dto모델 생성

---
+++ src/main/java/com/example/dating/dto/search/SearchDto.java
@@ -0,0 +1,16 @@
+package com.example.dating.dto.search;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SearchDto {
+
+    // 제목
+    private String title;
+
+    // 내용
+    private String content;
+
+}

8667b4122bb8c0c48aab87aa973eae58c2812229
qlsod <rlatpals100@naver.com>
2024-06-19 17:09:09 +0900

    DeviceToken redis 저장, 삭제 메소드 추가

--- src/main/java/com/example/dating/redis/service/RedisService.java
+++ src/main/java/com/example/dating/redis/service/RedisService.java
@@ -7,17 +7,34 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
-// RedisSerivce
 @Service
 @RequiredArgsConstructor
 public class RedisService {
 
-    private final RedisTemplate redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
     // 키-벨류 설정
     public void setValues(String token, String email){
         ValueOperations<String, String> values = redisTemplate.opsForValue();
         values.set(token, email, Duration.ofMinutes(30));
+    }
+
+    public void setDeviceToken(String deviceToken, String email) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+
+        if (redisTemplate.hasKey(email)) {
+            throw new RuntimeException("해당 deviceToken이 이미 존재합니다.");
+        }
+
+        values.set(email, deviceToken, Duration.ofMinutes(30));
+    }
+
+    public void delDeviceToken(String email) {
+        if (!redisTemplate.hasKey(email)) {
+            throw new RuntimeException("해당 deviceToken이 존재하지 않습니다.");
+        }
+
+        redisTemplate.delete(email);
     }
 
     // 키값으로 벨류 가져오기

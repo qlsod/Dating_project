a350e347e7e7864c8da6b8069036434ce837d24c
qlsod <rlatpals100@naver.com>
2024-06-19 17:09:34 +0900

    fcm Api 접속 위한 securityConfig 수정

--- src/main/java/com/example/dating/security/config/SecurityConfig.java
+++ src/main/java/com/example/dating/security/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                                 .antMatchers("/member/mail/confirm").permitAll()
                                 .antMatchers("/refresh").permitAll()
                                 .antMatchers("/ws/**").permitAll()
+                                .antMatchers("/fcm").permitAll()
                                 .antMatchers(HttpMethod.GET, "/search/**").permitAll() // GET 요청 허용
                                 .antMatchers("/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
                                         "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",

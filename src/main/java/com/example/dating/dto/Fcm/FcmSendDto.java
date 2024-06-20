24f679a9f73422b6dbb37177d731135a57380f09
qlsod <rlatpals100@naver.com>
2024-06-20 17:06:38 +0900

    FcmSendDto Valid 추가 및 UUID -> Name으로 변경

--- src/main/java/com/example/dating/dto/Fcm/FcmSendDto.java
+++ src/main/java/com/example/dating/dto/Fcm/FcmSendDto.java
@@ -3,21 +3,21 @@ package com.example.dating.dto.Fcm;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "/fcm RequestDto")
 public class FcmSendDto {
-    private Long targetUserId;
 
+    @NotEmpty(message = "상대방의 이름을 입력해주세요")
+    private String targetName;
+
+    @NotEmpty(message = "알림 제목을 입력해주세요")
     private String title;
 
+    @NotEmpty(message = "알림 내용을 입력해주세요")
     private String body;
 
-    @Builder(toBuilder = true)
-    public FcmSendDto(Long targetUserId, String title, String body) {
-        this.targetUserId = targetUserId;
-        this.title = title;
-        this.body = body;
-    }
 }

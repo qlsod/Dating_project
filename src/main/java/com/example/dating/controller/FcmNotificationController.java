b44e2212a979f5866645ac74131ece2ed20a0de0
qlsod <rlatpals100@naver.com>
2024-06-20 15:40:44 +0900

    스웨거 정보 추가

--- src/main/java/com/example/dating/controller/FcmNotificationController.java
+++ src/main/java/com/example/dating/controller/FcmNotificationController.java
@@ -2,6 +2,10 @@ package com.example.dating.controller;
 
 import com.example.dating.dto.Fcm.FcmSendDto;
 import com.example.dating.service.FcmService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +21,13 @@ public class FcmNotificationController {
 
     private final FcmService fcmService;
 
+
+    @Operation(summary = "FCM 알림 보내기",
+            description = "채팅, 좋아요 표시 알림")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "실패")
+    })
     @PostMapping("")
     public ResponseEntity<Void> sendNotificationByToken(@RequestBody FcmSendDto fcmSendDto) {
 

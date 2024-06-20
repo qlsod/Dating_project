1954330047cc40b6c633cd871ae114cff3f01080
qlsod <rlatpals100@naver.com>
2024-06-20 16:13:12 +0900

    logout Api securityConfig 수정

--- src/main/java/com/example/dating/security/config/SecurityConfig.java
+++ src/main/java/com/example/dating/security/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                                 .antMatchers("/refresh").permitAll()
                                 .antMatchers("/ws/**").permitAll()
                                 .antMatchers("/fcm").permitAll()
+                                .antMatchers("/member/logout").permitAll()
                                 .antMatchers(HttpMethod.GET, "/search/**").permitAll() // GET 요청 허용
                                 .antMatchers("/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
                                         "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",

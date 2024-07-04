7ef2e425c50bb0afd5d4016210a003d6f819bfbe
qlsod <rlatpals100@naver.com>
2024-07-04 19:50:59 +0900

    프로젝트 단 시간대 설정(KST)

--- src/main/java/com/example/dating/DatingApplication.java
+++ src/main/java/com/example/dating/DatingApplication.java
@@ -6,13 +6,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class DatingApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DatingApplication.class, args);
 	}
+	// Bean 생명주기를 이용한 timezone 설정
 
 	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");

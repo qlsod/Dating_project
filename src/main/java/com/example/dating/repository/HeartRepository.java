1e7ebcd4c53e78408907a96d7064ce232db8d5d0
qlsod <rlatpals100@naver.com>
2024-08-02 21:42:54 +0900

    MainHome 화면 API Dto 변경에 따른 Query문 수정

--- src/main/java/com/example/dating/repository/HeartRepository.java
+++ src/main/java/com/example/dating/repository/HeartRepository.java
@@ -3,6 +3,7 @@ package com.example.dating.repository;
 import com.example.dating.domain.Heart;
 import com.example.dating.domain.Member;
 import com.example.dating.dto.heart.HeartMemberDto;
+import com.example.dating.dto.member.MemberCommonDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,13 +12,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
-    @Query("select new com.example.dating.dto.heart.HeartMemberDto(m.id, m.nickName, m.age) " +
+    @Query("select new com.example.dating.dto.member.MemberCommonDto(m.id, m.birthDay, m.nickName, m.description, m.gender, m.address, " +
+            "m.age, m.height, m.image, m.personalInfo, m.personality, m.interest, m.likePersonality) " +
             "from Heart h left join Member m on h.receiver = m where h.sender.email = :email order by rand()")
-    List<HeartMemberDto> findFiveRandomMemberBySender(@Param("email") String email, Pageable pageable);
+    List<MemberCommonDto> findFiveRandomMemberBySender(@Param("email") String email, Pageable pageable);
 
-    @Query("select new com.example.dating.dto.heart.HeartMemberDto(m.id, m.nickName, m.age) " +
+    @Query("select new com.example.dating.dto.member.MemberCommonDto(m.id, m.birthDay, m.nickName, m.description, m.gender, m.address, " +
+            "m.age, m.height, m.image, m.personalInfo, m.personality, m.interest, m.likePersonality) " +
             "from Heart h left join Member m on h.sender = m where h.receiver.email = :email order by rand()")
-    List<HeartMemberDto> findFiveRandomMemberByReceiver(@Param("email") String email, Pageable pageable);
+    List<MemberCommonDto> findFiveRandomMemberByReceiver(@Param("email") String email, Pageable pageable);
 
     Integer countBySenderAndReceiver(Member sender, Member receiver);
 }

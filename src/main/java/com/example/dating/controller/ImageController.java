8c0e7f11af40f75a2eaf3846e93246c4185a5218
qlsod <rlatpals100@naver.com>
2024-08-22 17:05:53 +0900

    코드 정리

--- src/main/java/com/example/dating/controller/ImageController.java
+++ src/main/java/com/example/dating/controller/ImageController.java
@@ -11,9 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor

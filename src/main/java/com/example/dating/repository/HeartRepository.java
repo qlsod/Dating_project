1cfdb895db578b57580f995f6c0c3f081e1cf5da
qlsod <rlatpals100@naver.com>
2024-08-23 16:48:35 +0900

    내가 하트 보낸 사람 리스트, 나한테 하트 보낸 사람 리스트 Paging 처리 위한 메소드 생성

--- src/main/java/com/example/dating/repository/HeartRepository.java
+++ src/main/java/com/example/dating/repository/HeartRepository.java
@@ -2,9 +2,6 @@ package com.example.dating.repository;
 
 import com.example.dating.domain.Heart;
 import com.example.dating.domain.Member;
-import com.example.dating.dto.heart.HeartMemberDto;
-import com.example.dating.dto.member.MemberCommonDto;
-import com.example.dating.dto.member.MemberInfoDto;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,14 +21,21 @@ public interface HeartRepository extends JpaRepository<Heart, Long> {
 //            "from Heart h left join Member m on h.sender = m where h.receiver.email = :email order by rand()")
 //    List<MemberCommonDto> findFiveRandomMemberByReceiver(@Param("email") String email, Pageable pageable);
 
-    @Query(value = "select m from Heart h left join Member m on h.sender = m where h.receiver.email = :email order by h.id desc")
+    @Query("select m from Heart h left join Member m on h.sender = m where h.receiver.email = :email order by h.id desc")
     List<Member> findByReceiver(@Param("email") String email, PageRequest pageRequest);
 
-    @Query(value = "select m from Heart h left join Member m on h.sender = m where h.receiver.email = :email and h.id > :id order by h.id desc")
+    @Query("select h.sender from Heart h left join Member m on h.sender = m " +
+            "where h.receiver.email = :email " +
+            "and h.id < (select h2.id from Heart h2 where h2.sender.id = :id) order by h.id desc")
     List<Member> findPagingMemberByReceiver(@Param("email") String email, @Param("id") Long id, PageRequest pageRequest);
 
-    @Query(value = "select m from Heart h left join Member m on h.receiver = m where h.sender.email = :email order by rand()")
-    List<Member> findFiveRandomMemberBySender(@Param("email") String email, Pageable pageable);
+
+    @Query("select h.receiver from Heart h left join Member m on h.sender = m " +
+            "where h.sender.email = :email " +
+            "and h.id < (select h2.id from Heart h2 where h2.receiver.id = :id) order by h.id desc")
+    List<Member> findPagingMemberBySender(@Param("email") String email, @Param("id") Long id, PageRequest pageRequest);
+    @Query(value = "select m from Heart h left join Member m on h.receiver = m where h.sender.email = :email order by h.id desc")
+    List<Member> findMemberBySender(@Param("email") String email, Pageable pageable);
 
     Integer countBySenderAndReceiver(Member sender, Member receiver);
 }

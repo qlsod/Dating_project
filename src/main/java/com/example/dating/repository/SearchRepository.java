da0c5b4992df90ea78e5e48c679cd7067721d8ac
qlsod <rlatpals100@naver.com>
2024-09-18 21:15:29 +0900

    Search 수정 API 생성으로 인한 코드 수정

--- src/main/java/com/example/dating/repository/SearchRepository.java
+++ src/main/java/com/example/dating/repository/SearchRepository.java
@@ -1,7 +1,7 @@
 package com.example.dating.repository;
 
-import com.example.dating.domain.ProfileImage;
 import com.example.dating.domain.Search;
+import com.example.dating.dto.search.SearchHistoryRes;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,6 +21,12 @@ public interface SearchRepository extends JpaRepository<Search, Long> {
     @Query("select s from Search s order by s.id desc")
     List<Search> findPagingSearchFirst(PageRequest pageRequest);
 
+    @Query("select s from Search s where s.member.email = :email and s.id = :id")
+    Search checkAuthor(@Param("email") String email, @Param("id") Long id);
+
+
+    @Query("select new com.example.dating.dto.search.SearchHistoryRes(s.id, s.title, s.content, s.createdAt) from Search s where s.member.nickName = :nickName order by s.createdAt desc")
+    List<SearchHistoryRes> findSearchHistory(@Param("nickName") String nickName);
 
 }
 

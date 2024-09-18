ed7a67055f5374f65e63b3ccd8947682d2bf4a53
qlsod <rlatpals100@naver.com>
2024-09-18 21:15:51 +0900

    Search 수정 API 생성으로 인한 코드 수정

--- src/main/java/com/example/dating/dto/search/SearchDto.java
+++ src/main/java/com/example/dating/dto/search/SearchDto.java
@@ -3,9 +3,6 @@ package com.example.dating.dto.search;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.Column;
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
@@ -23,5 +20,6 @@ public class SearchDto {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt; // 보낸 시간
+
 
 }

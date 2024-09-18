8de14168ec8b753a3eb677aeb8145f775f3f0484
qlsod <rlatpals100@naver.com>
2024-09-18 21:14:48 +0900

    Search 닉네임 이용한 list 불러오기 API 생성 위한 모델 수정

---
+++ src/main/java/com/example/dating/dto/search/SearchHistoryRes.java
@@ -0,0 +1,22 @@
+package com.example.dating.dto.search;
+
+
+import com.example.dating.domain.Search;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchHistoryRes {
+
+    private Long id;
+    private String title;
+    private String content;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt; // 보낸 시간
+}

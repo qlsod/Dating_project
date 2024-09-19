408bdf6f4146b2afa0eaf66134f96ff5655dc345
qlsod <rlatpals100@naver.com>
2024-09-19 18:55:26 +0900

    채팅방 목록 조회 시 읽음 여부 확인 위한 코드 수정

--- src/main/java/com/example/dating/repository/ChatRoomRepository.java
+++ src/main/java/com/example/dating/repository/ChatRoomRepository.java
@@ -24,30 +24,33 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     @Query("select c.uuid from ChatRoom c where c.id = :id")
     String findChatRoomUUID(@Param("id") Long id);
 
-    @Query("SELECT new com.example.dating.dto.chat.ChatListDto(c.id, c.otherMember.nickName, c.otherMember.image, cm.message, cm.createdAt) " +
+    @Query("SELECT new com.example.dating.dto.chat.ChatListDto(c.id, c.otherMember.nickName, c.otherMember.image, cm.message, cm.createdAt, COALESCE(cr.isRead, false)) " +
             "FROM ChatRoom c " +
             "JOIN ChatMessage cm ON c.id = cm.chatRoomId " +
+            "LEFT JOIN ChatRead cr ON c.id = cr.chatRoom.id AND cr.userId = (SELECT m.id FROM Member m WHERE m.email = :email) " +
             "WHERE cm.createdAt = (SELECT MAX(cm2.createdAt) FROM ChatMessage cm2 WHERE cm2.chatRoomId = c.id) " +
             "AND c.member.email = :email " +
             "AND c.type = :type " +
             "AND (c.member.nickName = cm.nickName OR c.otherMember.nickName = cm.nickName) " +
             "AND c.otherMember.id NOT IN (SELECT b.blockMember.id FROM Block b WHERE b.blockItMember.email = :email) " +
-            "GROUP BY c.id, c.otherMember.nickName, c.otherMember.image, cm.message, cm.createdAt " +
+            "GROUP BY c.id, c.otherMember.nickName, c.otherMember.image, cm.message, cm.createdAt, cr.isRead " +
             "ORDER BY cm.createdAt DESC")
     List<ChatListDto> findListByMember(@Param("email") String email, @Param("type") String type);
 
 
-    @Query("SELECT new com.example.dating.dto.chat.ChatListDto(c.id, c.member.nickName, c.member.image, cm.message, cm.createdAt) " +
+    @Query("SELECT new com.example.dating.dto.chat.ChatListDto(c.id, c.member.nickName, c.member.image, cm.message, cm.createdAt, COALESCE(cr.isRead, false)) " +
             "FROM ChatRoom c " +
             "JOIN ChatMessage cm ON c.id = cm.chatRoomId " +
+            "LEFT JOIN ChatRead cr ON c.id = cr.chatRoom.id AND cr.userId = (SELECT m.id FROM Member m WHERE m.email = :email) " +
             "WHERE cm.createdAt = (SELECT MAX(cm2.createdAt) FROM ChatMessage cm2 WHERE cm2.chatRoomId = c.id) " +
             "AND c.otherMember.email = :email " +
             "AND c.type = :type " +
             "AND (c.member.nickName = cm.nickName OR c.otherMember.nickName = cm.nickName) " +
             "AND c.member.id NOT IN (SELECT b.blockMember.id FROM Block b WHERE b.blockItMember.email = :email) " +
-            "GROUP BY c.id, c.member.nickName, c.member.image, cm.message, cm.createdAt " +
+            "GROUP BY c.id, c.member.nickName, c.member.image, cm.message, cm.createdAt, cr.isRead " +
             "ORDER BY cm.createdAt DESC")
     List<ChatListDto> findListByOtherMember(@Param("email") String email, @Param("type") String type);
+
 
 
     Optional<ChatRoom> findChatRoomById(@Param("id") Long id);

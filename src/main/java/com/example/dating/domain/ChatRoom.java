b656262723cc98f610d148822e0f818f3560d785
qlsod <rlatpals100@naver.com>
2024-09-26 19:24:11 +0900

    @AllArgsConstructor 추가

--- src/main/java/com/example/dating/domain/ChatRoom.java
+++ src/main/java/com/example/dating/domain/ChatRoom.java
@@ -1,5 +1,6 @@
 package com.example.dating.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ChatRoom {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CHATROOM_ID")

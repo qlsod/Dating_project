26b272cab45d0a6c75b72458c51e8b7ccee8bf40
qlsod <rlatpals100@naver.com>
2024-09-26 19:24:02 +0900

    @AllArgsConstructor 추가

--- src/main/java/com/example/dating/dto/chat/ChatMessageDto.java
+++ src/main/java/com/example/dating/dto/chat/ChatMessageDto.java
@@ -1,6 +1,7 @@
 package com.example.dating.dto.chat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessageDto {
 
     public enum MessageType {
